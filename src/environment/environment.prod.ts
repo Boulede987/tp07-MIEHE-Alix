ddb0de909cb1a0cfca94454a88fd539f7b3080d8
MIEHE Alix <alixmiehe2004@gmail.com>
2025-12-29 20:55:27 +0100

    forgot ti update prod environment variables

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
   production: false,
   backendClient: backendClient,
   listPollution: backendClient + '/api/pollution',
-  listUsers: backendClient + '/api/user'
+  listUsers: backendClient + '/api/user',
+  loginUser: backendClient + '/api/user/login'
 };
